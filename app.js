98af5c9e617be050d1da3e3a8ac2ae36fa8b9fae
Doug Gandle <doug.gandle@gmail.com>
2017-04-04 12:21:56 -0400

    link database to server

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 var express = require('express')
 var app = express()
+var path = require('path')
+var bodyParser = require('body-parser')
 var mongoDB = 'mongodb://admin:password@ds151820.mlab.com:51820/cs496';
+var MongoClient = require('mongodb').MongoClient
 
-app.get('/', function (req, res) {
- res.send('Hello World!')
-})
+var db
 
-app.listen(3000, function () {
- 		console.log('Example app listening on port 3000!')
+MongoClient.connect(mongoDB, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+
+  app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
+
+  app.use(bodyParser.urlencoded({extended: true}))
+
+  app.get('/', function (req, res) {
+  //  res.send('Hello World!')
+    res.sendFile(path.join(__dirname + '/index.html'));
+  })
+
+  app.post('/users', function(req, res) {
+    db.collection('users').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+
+      console.log('saved to database')
+      res.redirect('/')
+    })
+  })
 })
